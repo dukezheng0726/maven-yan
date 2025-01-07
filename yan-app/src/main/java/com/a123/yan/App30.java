8c8d169cca8a70a36036ca738b2939980c049c66
unknown <duke.zheng0726@gmail.com>
2025-01-07 16:02:41 -0500

    20250107

--- yan-app/src/main/java/com/a123/yan/App30.java
+++ yan-app/src/main/java/com/a123/yan/App30.java
@@ -3,8 +3,8 @@ package com.a123.yan;
 /**
  * Hello world!
  */
-public class App3 {
+public class App30 {
     public static void main(String[] args) {
-        System.out.println("Hello yan3!");
+        System.out.println("Hello yan30!");
     }
 }
--- yan-app/src/test/java/com/a123/yan/App30Test.java
+++ yan-app/src/test/java/com/a123/yan/App30Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Unit test for simple App.
  */
-public class App3Test {
+public class App30Test {
 
     /**
      * Rigorous Test :-)
